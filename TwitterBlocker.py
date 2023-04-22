0c406747e5cef2ca6a599ccc268b5c4671ea7e58
Sarah Eaglesfield <speedbunny@gmail.com>
2023-04-22 15:34:47 +0100

    Update TwitterBlocker.py

--- TwitterBlocker.py
+++ TwitterBlocker.py
@@ -3,6 +3,7 @@
 """
 Created on Sat Apr 22 05:36:13 2023
 @author: saraheaglesfield
+Twitter Block v1.0
 """
 import time
 import csv
@@ -24,7 +25,8 @@ def handle_rate_limit(response):
         wait_time = reset_time - int(time.time())
         print(f"Rate limit exceeded. Waiting for {wait_time} seconds.")
         time.sleep(wait_time + 1)
-
+        return True
+    return False
 # In your terminal please set your environment variables by running the following lines of code.
 # export 'CONSUMER_KEY'='<your_consumer_key>'
 # export 'CONSUMER_SECRET'='<your_consumer_secret>'
@@ -34,7 +36,7 @@ consumer_secret = os.environ.get("CONSUMER_SECRET")
 
 # Be sure to replace your-user-id with your own user ID or one of an authenticating user
 # You can find a user ID by using the user lookup endpoint
-id = "123456"
+id = "15777543"
 
 # Replace this line with the path to your CSV file containing user IDs
 csv_file_path = "/Volumes/Data/accounts.csv"
@@ -54,31 +56,33 @@ resource_owner_key = fetch_response.get("oauth_token")
 resource_owner_secret = fetch_response.get("oauth_token_secret")
 print("Got OAuth token: %s" % resource_owner_key)
 
-# Replace these lines with your access token & secret
-access_token = "xxxxxxxxxxxx"
-access_token_secret = "xxxxxxxxxxxx"
+access_token = "15777543-DeuQIPMVhjmeOzlkM9U2n8kbDlc2ziYMIYKlH21fh"
+access_token_secret = "betj2gJp4LTCeW0WHJjPHMP79Z6rS9lcoiyLpNusS9PsG"
 print("Got Access token: %s" % resource_owner_key)
 
 
 for target_user_id in user_ids:
     payload = {"target_user_id": str(target_user_id)}
     
-    oauth = OAuth1Session(
-        consumer_key,
-        client_secret=consumer_secret,
-        resource_owner_key=access_token,
-        resource_owner_secret=access_token_secret,
-    )
-    response = oauth.post(
-        "https://api.twitter.com/2/users/{}/blocking".format(id), json=payload
-    )
-    
-    if response.status_code != 200:
-        print(
-            "Error blocking user {}: {} {}".format(
-                target_user_id, response.status_code, response.text
-            )
+    while True:
+        oauth = OAuth1Session(
+            consumer_key,
+            client_secret=consumer_secret,
+            resource_owner_key=access_token,
+            resource_owner_secret=access_token_secret,
         )
-        handle_rate_limit(response)
-    else:
-        print("Successfully blocked user {}".format(target_user_id))
+        response = oauth.post(
+            "https://api.twitter.com/2/users/{}/blocking".format(id), json=payload
+        )
+        
+        if response.status_code != 200:
+            print(
+                "Error blocking user {}: {} {}".format(
+                    target_user_id, response.status_code, response.text
+                )
+            )
+            if not handle_rate_limit(response):
+                break
+        else:
+            print("Successfully blocked user {}".format(target_user_id))
+            break
